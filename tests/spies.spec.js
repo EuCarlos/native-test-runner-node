e8607ac3f7e4c9144711ec39d227011a878c3b9e
Carlos Alves <carlosalves369m@hotmail.com>
2023-05-15 15:47:36 -0300

    add tests (spies and stubs)

---
+++ tests/spies.spec.js
@@ -0,0 +1,22 @@
+import { describe, it, mock } from 'node:test';
+import assert from 'node:assert'
+
+function run({ fn, times }) {
+    for(let i = 0; i <= times; i++) {
+        fn({ current: i * 5 })
+    }
+}
+
+describe('Spies test suite', () => {
+    it('should verify calls in a mock', () => {
+        const spy = mock.fn()
+        run({ fn: spy, times: 2 })
+        
+        assert.strictEqual(spy.mock.callCount(), 3)
+        
+        const calls = spy.mock.calls
+        assert.deepStrictEqual(calls[0].arguments, [{ current: 0 }])
+        assert.deepStrictEqual(calls[1].arguments, [{ current: 5 }])
+        assert.deepStrictEqual(calls[2].arguments, [{ current: 10 }])
+      })
+})
